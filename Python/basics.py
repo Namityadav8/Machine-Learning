552cb65f18a27a11cdaf58deaf31c2f87c0a9d00
Namit <namityadav008@gmail.com>
2024-09-04 22:57:59 +0530

    made some changes

---
+++ Python/basics.py
@@ -0,0 +1,25 @@
+# l1 = ["eat", "sleep", "repeat"]
+
+# for a,b in enumerate(l1):
+#     print (a,b)
+
+
+# a=True
+# b=False
+# print(a or b)
+
+
+
+# a="hello there is this guy"
+
+# # print(a.replace("there","where"))
+# # print(a.find("there"))
+
+# # print(a[:5])
+
+# a=3.32452345
+a=[[1,2,3],["nbamit","asf"],4,True]
+
+
+
+
