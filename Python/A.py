0c6048f20b2e402473d77bef7fed949163d15454
Namit <namityadav008@gmail.com>
2025-01-14 23:25:17 +0530

    ML

--- Python/A.py
+++ Python/A.py
@@ -1,9 +1,13 @@
-a=int(input("ENter the Size of your array"))
-b=[]
-for i in range(a):
-    elemet=input()
-    b.append(elemet)
+# a=int(input("ENter the Size of your array"))
+# b=[]
+# for i in range(a):
+#     elemet=input()
+#     b.append(elemet)
 
-for i in range(a):
-    print(b[i])
+# for i in range(a):
+#     print(b[i])
 
+
+a=23
+b=231
+print(round(3.14*a*b,1) )
--- Python/Ineritance.py
+++ Python/Ineritance.py
@@ -1,25 +1,53 @@
-class baap:
-    def __init__(self,name,age,childs):
-        self.name = name
-        self.age = age
-        self.childs = childs
+# #single inheritane 
+# class baap:
+#     def __init__(self,name,age,childs):
+#         self.name = name
+#         self.age = age
+#         self.childs = childs
     
-    def parent(self):
-        print(f"hello this is {self.name}")
-        print(f"{self.name} has age {self.age}")
+#     def parent(self):
+#         print(f"hello this is {self.name}")
+#         print(f"{self.name} has age {self.age}")
         
 
 
-class child(baap):
-    def __init__(self,name, age, childs,brothers):
-        super().__init__(name , age,childs)
-        self.brothers = brothers
+# class child(baap):
+#     def __init__(self,name, age, childs,brothers):
+#         super().__init__(name , age,childs)
+#         self.brothers = brothers
     
-    def bhai(self):
-        print(f"{self.name} has  brothers")
+#     def bhai(self):
+#         print(f"{self.name} has  brothers")
 
-bache = child("namit",20,2,0)
-pita = baap("raj",50,2)
+# bache = child("namit",20,2,0)
+# pita = baap("raj",50,2)
 
-bache.bhai()
-bache.parent()  # calling the function of parent class
+# bache.bhai()
+# bache.parent()  # calling the function of parent class
+
+
+#Multiple Inheritance
+
+class dad:
+    def __init__(self,name):
+        self.name = name
+
+    def nam(self):
+        print(f"hello this is {self.name}")
+    
+class mom:
+    def __init__(self,age):
+        self.age = age
+
+    def umar(self):
+        print(f"{self.age} is the age of mom")
+
+class house(dad,mom):
+    def __init__(self, name,age):
+        dad.__init__(self,name)
+        mom.__init__(self,age)
+
+ghar = house('raj',50)
+
+ghar.nam()
+ghar.umar()
