da4d0466bd2ba51163fcd737c48e670483033a9e
Namit <namityadav008@gmail.com>
2025-01-16 21:05:16 +0530

    Pandas

--- Libraries/numpy/vector1.py
+++ Libraries/numpy/vector1.py
@@ -15,8 +15,9 @@ import numpy as np
 
 
 
-arr = np.array([1, 2, 3, 4, 5, 6])
-array1=np.split(arr,2)
+arr = np.array([1, 2, 3, 4, 5, 6,1,2,3])
+print(np.unique(arr))
 array = np.array_split(arr, 3)
 print(array)
-print(array1)
+# array1=np.split(arr,2)
+# print(array1)
