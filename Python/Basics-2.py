121e6b67f99a8c689e9521715cef415c9005dd14
Namit <namityadav008@gmail.com>
2025-01-16 15:20:43 +0530

    starting numpy

--- Python/Basics-2.py
+++ Python/Basics-2.py
@@ -1,5 +1,13 @@
-a=[1,2,3,4]
-b=iter(a)
-print(next(b))
-print(next(b))
+# a=[1,2,3,4]
+# b=iter(a)
+# print(next(b))
+# print(next(b))
 
+# def read_file(file_path):
+#     with open(file_path,'r') as file:
+#         for i in file:
+#             yield i
+
+
+a=[x for x in range(10)]     # List comprehension
+print(a)
